c7ab6338675fe948a64c0646d861ec3eb8295366
Manuel <salomanuel.aux+code@gmail.com>
2016-10-29 20:11:00 +0200

    mark method works

--- exercises/knight1.rb
+++ exercises/knight1.rb
@@ -1,15 +1,24 @@
 class ChessBoard
 	def initialize
-		a = 0
-		b = 1
-		c = 2
-		d = 3
-		e = 4
-		f = 5
-		g = 6
-		h = 7
+		@a = 0
+		@b = 1
+		@c = 2
+		@d = 3
+		@e = 4
+		@f = 5
+		@g = 6
+		@h = 7
+		@oldX  = nil
+		@oldY  = nil
 		@board = []
 		8.times { @board << Array.new(8, ".") }
+	end
+
+	def mark(x, y)
+		@board[@oldY][@oldX] = "o" if !@oldY.nil?
+		@board[y][x]       = "X"
+		@oldX = x
+		@oldY = y
 	end
 
 	def show_board
