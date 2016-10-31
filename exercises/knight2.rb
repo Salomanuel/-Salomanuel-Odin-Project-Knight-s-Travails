7596460a2c523de89c4560182ab4e0041ed7fcaf
Manuel <salomanuel.aux+code@gmail.com>
2016-10-31 18:20:29 +0100

    moves_range works brilliantly

--- exercises/knight2.rb
+++ exercises/knight2.rb
@@ -24,25 +24,23 @@ class Knight_Travels
 		end
 
 		def moves_range(ary,option)
-			puts "#{ary.join}, #{option}, #{[ary[0]+=1, ary[1]-=2]}"
 			case option
 				when 0 then return [ary[0]+=1, ary[1]-=2]
-			# 	when 1 then return [ary[0]+=2, ary[1]-=1]
-			# 	when 2 then return [ary[0]+=2, ary[1]+=1]
-			# 	when 3 then return [ary[0]+=1, ary[1]+=2]
-			# 	when 4 then return [ary[0]-=1, ary[1]+=2]
-			# 	when 5 then return [ary[0]-=2, ary[1]+=1]
-			# 	when 6 then return [ary[0]-=2, ary[1]-=1]
-			# 	when 7 then return [ary[0]-=1, ary[1]-=2]
+				when 1 then return [ary[0]+=2, ary[1]-=1]
+				when 2 then return [ary[0]+=2, ary[1]+=1]
+				when 3 then return [ary[0]+=1, ary[1]+=2]
+				when 4 then return [ary[0]-=1, ary[1]+=2]
+				when 5 then return [ary[0]-=2, ary[1]+=1]
+				when 6 then return [ary[0]-=2, ary[1]-=1]
+				when 7 then return [ary[0]-=1, ary[1]-=2]
 			end
-			horse.show_board
 		end
 
 		def circle_run_test
 			8.times do |i|
 				# knight_moves(moves_range(@knight_position,i))
-				# knight_moves(moves_range([3,4],i))
-				knight_moves([4,2])
+				knight_moves(moves_range([3,4],i))
+				# knight_moves([4,2])
 			end
 		end
 	end
