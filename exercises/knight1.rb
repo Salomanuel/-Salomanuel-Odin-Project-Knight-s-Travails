73a1e243c0bf42b13fbf34cc772143daa85b8057
Manuel <salomanuel.aux+code@gmail.com>
2016-10-31 12:18:52 +0100

    ordered all in modules and undressed main class from  initialize and some other methods

--- exercises/knight1.rb
+++ exercises/knight1.rb
@@ -1,32 +1,45 @@
 class ChessBoard
+	module Board
+		def create_board
+			@board 		 = 8.times.map { Array.new(8, ".") }
+		end
+
+		def mark(x, y, old=false)	# DRAWS ON THE BOARD
+			old ? @board[y][x] = "o" :	@board[y][x] = "X" 
+		end
+
+		def show_board
+			puts ("A".."H").to_a.join(" ")
+			@board.each_with_index { |line, i| puts "#{line.join(" ")} #{i}" }
+		end
+	end
+	module Input
+		def input(x,y)						# handles old and new markings
+			@old_moves ||= []
+			x = letters(x) if x.is_a?(String)
+			if @old_moves.length > 0
+				old = @old_moves.last 
+				mark(old[0],old[1],true)
+			end
+			@old_moves << [x,y]
+			mark(x,y)
+		end
+
+		def letters(letter)				# WHEN SOME INPUT IS A LETTER
+			return ("a".."h").to_a.index(letter.downcase)
+		end
+	end
+	include Board
+	include Input
+
 	def initialize
-		@old_moves = []
-		@board 		 = 8.times.map { Array.new(8, ".") }
+		create_board
 		input(3,4)							# JUST TO CALL MARK
 	end
 
-	def input(x,y)						# handles old and new markings
-		x = letters(x) if x.is_a?(String)
-		if @old_moves.length > 0
-			old = @old_moves.last 
-			mark(old[0],old[1],true)
-		end
-		@old_moves << [x,y]
-		mark(x,y)
-	end
+	
 
-	def letters(letter)				# WHEN SOME INPUT IS A LETTER
-		return ("a".."h").to_a.index(letter.downcase)
-	end
-
-	def mark(x, y, old=false)	# DRAWS ON THE BOARD
-		old ? @board[y][x] = "o" :	@board[y][x] = "X" 
-	end
-
-	def show_board
-		puts ("A".."H").to_a.join(" ")
-		@board.each_with_index { |line, i| puts "#{line.join(" ")} #{i}" }
-	end
+	
 
 	# def check_validity
 	# 	return false if @x > 7
