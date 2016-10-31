a6f170f88b8ee8fc22674814ac63fbddaa447a11
Manuel <salomanuel.aux+code@gmail.com>
2016-10-31 17:22:06 +0100

    add @knight_position variable and trail of the knight

--- exercises/knight2.rb
+++ exercises/knight2.rb
@@ -1,4 +1,5 @@
 class Knight_Travels
+	# => X and Y are INVERTED: @board[y][x]
 	module Board
 		def build_board
 			@board = 8.times.map { Array.new(8, ".") }
@@ -11,11 +12,24 @@ class Knight_Travels
 		end
 	end
 
+
 	include Board
 
 	def initialize
 		build_board
-		show_board
+		@knight_position = [3,4]
+		# show_board
+	end
+
+	def move(x,y,trail=false)		# => CALLED BY KNIGHT_MOVES
+		trail ? @board[y][x] = "o" : @board[y][x] = "X"
+	end
+
+	def knight_moves(x,y)
+		kp = @knight_position
+		move(kp[0],kp[1],true)
+		@knight_position = [x,y]
+		move(x,y)
 	end
 
 
@@ -23,3 +37,8 @@ class Knight_Travels
 end
 
 horse = Knight_Travels.new
+# horse.move(2,3,true)
+horse.knight_moves(2,3)
+horse.knight_moves(2,1)
+
+horse.show_board
