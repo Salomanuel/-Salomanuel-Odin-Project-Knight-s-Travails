cc8fcd51cd154c5f89c17e7d50d66a7514b0aba7
Manuel <salomanuel.aux+code@gmail.com>
2016-10-31 18:13:30 +0100

    update knight_moves, it was still using not array outputs

--- exercises/knight2.rb
+++ exercises/knight2.rb
@@ -18,21 +18,31 @@ class Knight_Travels
 
 		def knight_moves(ary)				# => calls move and handles the trail
 			kp = @knight_position
-			move(kp[0],kp[1],true)
+			move(kp,true)
 			@knight_position = ary 		# => also updates @knight_position
-			move(ary[0],ary[1])
+			move(ary)
 		end
 
-		def moves_range(x,y,option)
+		def moves_range(ary,option)
+			puts "#{ary.join}, #{option}, #{[ary[0]+=1, ary[1]-=2]}"
 			case option
-				when 0 then return [x+=1, y-=2]
-				when 1 then return [x+=2, y-=1]
-				when 2 then return [x+=2, y+=1]
-				when 3 then return [x+=1, y+=2]
-				when 4 then return [x-=1, y+=2]
-				when 5 then return [x-=2, y+=1]
-				when 6 then return [x-=2, y-=1]
-				when 7 then return [x-=1, y-=2]
+				when 0 then return [ary[0]+=1, ary[1]-=2]
+			# 	when 1 then return [ary[0]+=2, ary[1]-=1]
+			# 	when 2 then return [ary[0]+=2, ary[1]+=1]
+			# 	when 3 then return [ary[0]+=1, ary[1]+=2]
+			# 	when 4 then return [ary[0]-=1, ary[1]+=2]
+			# 	when 5 then return [ary[0]-=2, ary[1]+=1]
+			# 	when 6 then return [ary[0]-=2, ary[1]-=1]
+			# 	when 7 then return [ary[0]-=1, ary[1]-=2]
+			end
+			horse.show_board
+		end
+
+		def circle_run_test
+			8.times do |i|
+				# knight_moves(moves_range(@knight_position,i))
+				# knight_moves(moves_range([3,4],i))
+				# knight_moves([4,2])
 			end
 		end
 	end
@@ -45,16 +55,14 @@ class Knight_Travels
 		move(@knight_position)		# => the first time gets kickstarted to bypass the trail
 		# show_board
 	end
-
-
-
-
-
 end
 
 horse = Knight_Travels.new
 # horse.move(2,3,true)
 # horse.knight_moves(2,3)
 # horse.knight_moves(2,1)
+horse.knight_moves([2,1])
+# horse.move([2,3],true)
 
 horse.show_board
+# horse.circle_run_test
