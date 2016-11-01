402710a4ee6fb896e5fea20ddf990915cc228630
Manuel <salomanuel.aux+code@gmail.com>
2016-11-01 16:03:47 +0100

    fix the moves_range method, which used +=n instead of +n

--- exercises/knight2.rb
+++ exercises/knight2.rb
@@ -25,21 +25,42 @@ class Knight_Travels
 
 		def moves_range(ary,option)
 			case option
-				when 0 then return [ary[0]+=1, ary[1]-=2]
-				when 1 then return [ary[0]+=2, ary[1]-=1]
-				when 2 then return [ary[0]+=2, ary[1]+=1]
-				when 3 then return [ary[0]+=1, ary[1]+=2]
-				when 4 then return [ary[0]-=1, ary[1]+=2]
-				when 5 then return [ary[0]-=2, ary[1]+=1]
-				when 6 then return [ary[0]-=2, ary[1]-=1]
-				when 7 then return [ary[0]-=1, ary[1]-=2]
+				when 0 then return [ary[0]+1, ary[1]-2]
+				when 1 then return [ary[0]+2, ary[1]-1]
+				when 2 then return [ary[0]+2, ary[1]+1]
+				when 3 then return [ary[0]+1, ary[1]+2]
+				when 4 then return [ary[0]-1, ary[1]+2]
+				when 5 then return [ary[0]-2, ary[1]+1]
+				when 6 then return [ary[0]-2, ary[1]-1]
+				when 7 then return [ary[0]-1, ary[1]-2]
 			end
 		end
 
+		def is_valid?(ary)
+			if (ary[0] > 7) or (ary[1] > 7)
+				return false 
+			elsif @board[ary[1]][ary[0]] == "o"
+				return false 
+			else
+				return true
+			end
+		end
+					# => IMPLEMENT CIRCLE RUN TEST WITH IS VALID
 		def circle_run_test
+			# knight_moves([2,1])
+			# knight_moves([3,2])
+			# puts @knight_position.join(",")
+			# puts moves_range(@knight_position,7).join(",")
+			# puts @knight_position.join(",")
+			# knight_moves(moves_range(@knight_position,7))
+			# knight_moves(moves_range([3,4],7))
+			# knight_moves(moves_range(@knight_position,7))
 			8.times do |i|
-				# knight_moves(moves_range(@knight_position,i))
-				knight_moves(moves_range([3,4],i))
+				move = moves_range(@knight_position,i)
+				if is_valid?(move)
+					knight_moves(move)
+				end
+				# knight_moves(moves_range([3,4],i))
 				# knight_moves([4,2])
 			end
 		end
@@ -51,6 +72,7 @@ class Knight_Travels
 		build_board
 		@knight_position = [3,4]
 		move(@knight_position)		# => the first time gets kickstarted to bypass the trail
+		@turn = 0
 		# show_board
 	end
 end
@@ -60,6 +82,7 @@ horse = Knight_Travels.new
 # horse.knight_moves(2,3)
 # horse.knight_moves(2,1)
 # horse.knight_moves([2,1])
+# horse.knight_moves([4,2])
 # horse.move([2,3],true)
 
 horse.circle_run_test
