c8f58b7ad4a84f9a154914550d15fd765855d598
Manuel <salomanuel.aux+code@gmail.com>
2016-11-01 18:02:47 +0100

    add descriptions all around

--- exercises/knight2.rb
+++ exercises/knight2.rb
@@ -1,5 +1,6 @@
 class Knight_Travels
 	# => X and Y are INVERTED: @board[y][x]
+	# => graphics
 	module Board
 		def build_board
 			@board = 8.times.map { Array.new(8, ".") }
@@ -8,24 +9,18 @@ class Knight_Travels
 		def show_board
 			puts (0..7).to_a.join("\t")
 			puts ("A".."H").to_a.join("\t")
-			@board.each_with_index { |line, i| puts "#{line.join("\t")} #{i} \n\n" }
-		end
-
-		def write_board(ary,trail=false)		# => CALLED BY KNIGHT_MOVES
-			# puts "#{ary.join(",")} turn: #{@turn}" if trail
-			trail ? @board[ary[1]][ary[0]] = @turn : @board[ary[1]][ary[0]] = "X"
-			@turn += 1 if trail
-		end
+			@board.each_with_index { |line, i| puts "#{line.join("\t")} (#{i} \n\n\n" }						# => handles the 								
 	end
+	# => mechanics
 	module Moving
-		def knight_moves(ary)				# => calls write_board and handles the trail
+		def knight_moves(ary)				# => handles the trail and calls write_board
 			kp = @knight_position
 			write_board(kp,true)
 			@knight_position = ary 		# => also updates @knight_position
 			write_board(ary)
 		end
 
-		def moves_range(ary,option)
+		def moves_range(ary,option) # => creates all the possible moves (even not valid)
 			case option
 				when 0 then return [ary[0]+1, ary[1]-2]
 				when 1 then return [ary[0]+2, ary[1]-1]
@@ -45,17 +40,17 @@ class Knight_Travels
 				return false 
 			else
 				return true
-			end
+			 				# => checks if the move is valid returning a boolean
 		end
-					# => IMPLEMENT CIRCLE RUN TEST WITH IS VALID
+	
 		def circle_run_test
-			20.times do |i|
+			7.times do |i|
 				move = moves_range(@knight_position,i)
 				if is_valid?(move)
 					knight_moves(move)
 				end
-			end
-		end
+			end 			# => just a test, it's not needed
+		end 							
 	end
 	include Board
 	include Moving
@@ -70,13 +65,13 @@ class Knight_Travels
 end
 
 horse = Knight_Travels.new
-# horse.move(2,3,true)
-# horse.knight_moves(2,3)
-# horse.knight_moves(2,1)
-# horse.knight_moves([2,1])
-# horse.knight_moves([4,2])
-# horse.move([2,3],true)
+#old tests
+	# horse.move(2,3,true)
+	# horse.knight_moves(2,3)
+	# horse.knight_moves(2,1)
+	# horse.knight_moves([2,1])
+	# horse.knight_moves([4,2])
+	# horse.move([2,3],true)
 
 horse.circle_run_test
-
 horse.show_board
