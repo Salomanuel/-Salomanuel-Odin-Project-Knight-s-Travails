c9a7e67f54adcb610aad22a253739cfc1a89ab31
Manuel <salomanuel.aux+code@gmail.com>
2016-11-03 14:05:01 +0100

    add recursion in .compare. Now it may work, I'm not brave enough to try it before lunch

--- exercises/quickKnight.rb
+++ exercises/quickKnight.rb
@@ -1,17 +1,21 @@
+TRY IF IT WORKS WITH MORE COMPLEX THINGS
+
+
 module QuickKnight
 
 	def sons(start_square) 						# => returns a list of the valid submoves
 		8.times.map do |i| 
 			move = moves_range(start_square, i)
 			move if is_valid?(move)
-		end
+		end.compact
 	end
 
-	def quickKnight(start,finish) 		# => it's a fake initialize
+	def quickInit(start,finish) 		# => it's a fake initialize
 		build_board  #temporary
 		@start   = start
 		@finish  = finish
 		@enders  = sons(@finish)
+		@checked = []
 	end
 
 	def has_enders?(square)  					# => checks if there are direct connection
@@ -20,8 +24,15 @@ module QuickKnight
 
 	def compare(squares=sons(@start)) # => calls the final string if finds direct connections
 		squares.each do |square| 
-			if has_enders?(square)
-				formatting_result(square) 
+			if @checked.include?(square)
+				return "all squares examined"
+			else
+				@checked << square
+				if has_enders?(square)
+					formatting_result(square) 
+				else
+					compare(sons(square))
+				end
 			end
 		end
 	end
@@ -43,11 +54,11 @@ include Moving
 include QuickKnight
 
 # quickKnight([4,4],[1,2])
-quickKnight([2,3],[1,2]) # => this has just one move
+quickInit([2,3],[1,2]) # => this has just one move
 # puts is_valid?([4,4])
 # starters = neighbors(@start)
 # enders = neighbors(@finish)
 # puts has_enders?([3,4])
-compare(sons([2,3]))
+puts compare(sons([2,3]))
 
 #.each { |s| puts s.join(",") }
