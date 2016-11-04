120e2010b3770dc6d0d1130a2bf02a0c7fe5b7fd
Manuel <salomanuel.aux+code@gmail.com>
2016-11-04 19:56:58 +0100

    input.rb gets better but still need refinement (doesn't handle mistakes that well yet)

--- input.rb
+++ input.rb
@@ -1,31 +1,39 @@
 module Input
 
-	def kind_questions	
-		puts "Hello fellow mortal, write down the starting square"
-		@start  = input
-		puts "Now just tell me the desired ending cell and we are done"
-		@finish = input
+	def kind_questions
+		@start, @finish = nil	
+		puts "\tHello fellow mortal, write down the starting square"
+		input(true)
+		puts "\tNow just tell me the desired ending cell and we are done"
+		input(false)
+		puts "#{@start} #{@finish}"
 	end
 
-	def input
-		print ">"
+	def input(start_or_finish)
+		# start is true, finish is false
+		print "\t>"
 		input  = gets.chomp.split("")
-		error  = "look, it's easy, the format is: \nA4\nwrite down something similar"
+		error  = "\tlook, it's easy, the format is: #{"\n\tA4".center(90)}\n\twrite down something similar"
 		output = []
 
-		if ("A".."H").include?(input[0].upcase)
+		# puts input.join("*")
+		# puts ("A".."H").to_a.include?(input[0].upcase)
+		# puts ("0".."7").to_a.include?(input[1])		
+		puts "#{@start} #{@finish}"
+		if ("A".."H").to_a.include?(input[0].upcase)
 			output << letters_to_numbers(input[0].upcase)
 		else
 			puts error
-			self.input
+			input(true)
 		end
-		if (0..7).include?(input[1].to_i)
+
+		if ("0".."7").to_a.include?(input[1])	 
 			output << input[1].to_i
 		else
 			puts error
-			self.input
+			input(false)
 		end
-		return output
+		start_or_finish ? @start = output : @finish = output
 	end
 
 	def letters_to_numbers(letter)
@@ -34,6 +42,6 @@ module Input
 
 end
 
-# include Input
-# puts kind_questions
+include Input
+puts kind_questions
 # # puts letters_to_numbers("H")
