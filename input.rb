400a5461a48effdd7a807a3e931a0aa59e67f9e6
Manuel <salomanuel.aux+code@gmail.com>
2016-11-05 13:06:22 +0100

    add REGEX check to input.rb, now it works good

--- input.rb
+++ input.rb
@@ -3,37 +3,25 @@ module Input
 	def kind_questions
 		@start, @finish = nil	
 		puts "\tHello fellow mortal, write down the starting square"
-		input(true)
-		puts "\tNow just tell me the desired ending cell and we are done"
-		input(false)
+		@start = get_input
+		puts "\tNow just tell me the desired ending cell\n\tand we are done"
+		@finish = get_input
 		puts "#{@start} #{@finish}"
 	end
 
-	def input(start_or_finish)
-		# start is true, finish is false
-		print "\t>"
-		input  = gets.chomp.split("")
-		error  = "\tlook, it's easy, the format is: #{"\n\tA4".center(90)}\n\twrite down something similar"
+	def get_input(mistake=false)	
+		error  = "\tlook, it's easy, the format is: \n\t\tA4\n\twrite down something similar"
 		output = []
-
-		# puts input.join("*")
-		# puts ("A".."H").to_a.include?(input[0].upcase)
-		# puts ("0".."7").to_a.include?(input[1])		
-		puts "#{@start} #{@finish}"
-		if ("A".."H").to_a.include?(input[0].upcase)
-			output << letters_to_numbers(input[0].upcase)
-		else
-			puts error
-			input(true)
-		end
-
-		if ("0".."7").to_a.include?(input[1])	 
+		puts error if mistake
+		print "\t>"
+		input  = gets.chomp.upcase
+		
+		if input =~ /[A-H][0-7]/ 
+			output << input[0].upcase
 			output << input[1].to_i
-		else
-			puts error
-			input(false)
+			return output
 		end
-		start_or_finish ? @start = output : @finish = output
+		self.get_input(true)
 	end
 
 	def letters_to_numbers(letter)
@@ -42,6 +30,6 @@ module Input
 
 end
 
-include Input
-puts kind_questions
+# include Input
+# puts kind_questions
 # # puts letters_to_numbers("H")
