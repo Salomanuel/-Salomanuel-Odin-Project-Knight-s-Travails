cccd8eda91175b32ccaf0e1a70f35209726b5615
Manuel <salomanuel.aux+code@gmail.com>
2016-11-05 13:41:38 +0100

    add create_board, show_board, draw_board

--- second_wind/knight3.rb
+++ second_wind/knight3.rb
@@ -11,11 +11,39 @@ class DamnHorse
 
 		def show_board
 			puts "  #{("A".."H").to_a.join(" ")}"
+			puts "  #{(0..7).to_a.join(" ")}"
 			@board.each_with_index { |line,i| puts "#{i} #{line.join(" ")}" }
+		end
+
+		def draw_board(cell=@start,type="S")
+			case type
+			when "S" then @board[cell[1]][cell[0]] = "S"
+			end
+		end
+	end
+
+	module Moving
+		def initialize
+			super
+		end
+
+		def moves_range(cell)
+			#cell
+		end
+	end
+
+	module Input
+		def initialize
+			super
+			# @start  = [4,4]
+			@start =  [3,4] # => D4
+			@finish = [1,2]
 		end
 	end
 
 	include Board
+	include Moving
+	include Input
 
 	def initialize
 		super 				# => calls the initialize of the modules
@@ -24,3 +52,5 @@ end
 
 horse = DamnHorse.new
 
+horse.draw_board
+horse.show_board
